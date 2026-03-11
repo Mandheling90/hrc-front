c5525000ca5127859ac6deeabe44c6f7b1cc94ea
Mandheling90 <du58ck@gmail.com>
2026-03-11 14:38:39 +0900

    핫라인 데이터 수정

--- src/app/network/hotline/page.tsx
+++ src/app/network/hotline/page.tsx
@@ -37,67 +37,67 @@ const mockHotlines: HotlineData[] = [
   {
     id: '4',
     department: '이식혈관외과',
-    professorName: '당직의',
+    professorName: '정철웅',
     hotline: '010-8993-5558'
   },
   {
     id: '5',
     department: '정형외과',
-    professorName: '김희중',
+    professorName: '양재혁',
     hotline: '010-7102-1534'
   },
   {
     id: '6',
     department: '신경외과 (뇌손상, 뇌출혈)',
-    professorName: '홍순철',
+    professorName: '당직의',
     hotline: '010-2010-8809'
   },
   {
     id: '7',
-    department: '심장혈관흉부외과 (대동맥, 혈관, 심장수술)',
-    professorName: '유성욱',
+    department: '심장혈관흉부외과\n(대동맥, 혈관, 심장수술)',
+    professorName: '김희중',
     hotline: '010-2323-5741'
   },
   {
     id: '8',
     department: '산부인과',
-    professorName: '최연조',
+    professorName: '홍순철',
     hotline: '010-4702-1065'
   },
   {
     id: '9',
     department: '신경과 (뇌졸중)',
-    professorName: '당직교수',
+    professorName: '유성욱',
     hotline: '010-4599-4186'
   },
   {
     id: '10',
     department: '성형외과',
-    professorName: '최혁순',
+    professorName: '정재호',
     hotline: '010-4566-1030'
   },
   {
     id: '11',
     department: '소아청소년과',
-    professorName: '정철웅',
+    professorName: '이주영',
     hotline: '010-2693-4125'
   },
   {
     id: '12',
     department: '치과',
-    professorName: '양재혁',
+    professorName: '최연조',
     hotline: '010-7225-8988'
   },
   {
     id: '13',
     department: '응급의료센터',
-    professorName: '정재호',
+    professorName: '당직교수',
     hotline: '010-4605-6534'
   },
   {
     id: '14',
-    department: '진료협력센터장 / 소화기내과 (위장관)',
-    professorName: '이주영',
+    department: '진료협력센터장\n소화기내과 (위장관)',
+    professorName: '최혁순',
     hotline: '010-4909-0079'
   }
 ]
