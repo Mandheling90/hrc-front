f9a8365999d6d7b213f672c59d0938ecbd10541d
Mandheling90 <du58ck@gmail.com>
2026-03-12 10:12:48 +0900

    svg 오류 수정

--- src/components/icons/CloseIcon.tsx
+++ src/components/icons/CloseIcon.tsx
@@ -20,15 +20,15 @@ export const CloseIcon: React.FC<CloseIconProps> = ({
       <path
         d='M17.4167 34.0833C26.6217 34.0833 34.0833 26.6217 34.0833 17.4167C34.0833 8.21167 26.6217 0.75 17.4167 0.75C8.21167 0.75 0.75 8.21167 0.75 17.4167C0.75 26.6217 8.21167 34.0833 17.4167 34.0833Z'
         stroke='var(--gray-11)'
-        stroke-width='1.5'
-        stroke-linejoin='round'
+        strokeWidth='1.5'
+        strokeLinejoin='round'
       />
       <path
         d='M22.131 12.7021L12.7026 22.1305M12.7026 12.7021L22.131 22.1305'
         stroke='var(--gray-11)'
-        stroke-width='1.5'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   )
