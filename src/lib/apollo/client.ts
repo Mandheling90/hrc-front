99a3e1e5c0849f83c4dcd855d38da3ec0a6b0f67
LeeSeungYong <du58ck@gmail.com>
2026-03-21 19:12:29 +0900

    next api 설정

--- src/lib/apollo/client.ts
+++ src/lib/apollo/client.ts
@@ -45,7 +45,7 @@ function resolvePendingRequests() {
 }
 
 export function makeClient() {
-  const graphqlUri = process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:4000/graphql'
+  const graphqlUri = '/api/graphql'
 
   const authLink = new SetContextLink((prevContext, operation) => {
     const isConsultantOp = operation.operationName && CONSULTANT_OPERATIONS.includes(operation.operationName)
