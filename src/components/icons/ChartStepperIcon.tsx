c9ed8b923b1948bfd702cf88b8e468b84294fd67
LeeSeungYong <du58ck@gmail.com>
2026-03-25 22:24:45 +0900

    아이콘 오류 추가 수정

--- src/components/icons/ChartStepperIcon.tsx
+++ src/components/icons/ChartStepperIcon.tsx
@@ -17,7 +17,7 @@ export const ChartStepperIcon: React.FC<ChartStepperIconProps> = ({
     <svg xmlns='http://www.w3.org/2000/svg' width='60' height='60' viewBox='0 0 60 60' fill='none'>
       <path
         d='M26.25 41.25V26.25H41.25V11.25H56.25V7.5H37.5V22.5H22.5V37.5H7.5V3.75H3.75V52.5C3.75149 53.4941 4.14705 54.4471 4.84999 55.15C5.55293 55.8529 6.50589 56.2485 7.5 56.25H56.25V52.5H7.5V41.25H26.25Z'
-        fill='#5F1F27'
+        fill={stroke}
       />
     </svg>
   )
