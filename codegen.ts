7e964f4e474a5c2c36be2de7d1be2ad1cb82a903
jhw <jhw811@gmail.com>
2026-04-06 15:50:48 +0900

    codegen url 변경

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 const graphqlEndpoint =
   process.env.CODEGEN_SCHEMA_URL ||
   process.env.NEXT_PUBLIC_GRAPHQL_URL ||
-  'http://localhost:8000/graphql'
+  'http://api-service:8000/graphql'
 
 const hospitalCodeHeader =
   process.env.CODEGEN_HOSPITAL_CODE ||
